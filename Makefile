0447814d57b5517f00fa4009956e0d45c990c015
Your Name <you@example.com>
2015-01-23 12:43:27 +0000

    d

--- Makefile
+++ Makefile
@@ -12,8 +12,14 @@ FLAGS_C = -std=c99 $(WARN) -lpcre -I$(fish_util_dir) -I$(fish_utils_dir) $(fish_
 
 all: fish-screen
 
-fish-screen: main.c 
+fish-screen: fish-util fish-utils main.c 
 	gcc $(FLAGS_C) $(fish_util_o) $(fish_utils_o) main.c -o fish-screen
+
+fish-util: 
+	sh -c 'cd fish-lib-util/fish-util; make'
+
+fish-utils:
+	sh -c 'cd fish-lib-util/fish-utils; make'
 
 clean: 
 	rm -f *.o
--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #define LIMIT_HOSTNAME 20
 #define LIMIT_NAME_NEW 20
 
+#define VERSION_OLD 0x1
+#define VERSION_NEW 0x2
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,25 +28,25 @@
 #define sct(type, name) struct type name = {0};
 #define try(x) if(x) { \
         _();        \
-        const char* err = perr();       \
+        const char *err = perr();       \
         R(err);         \
         warn("Error: %s", _s);         \
     };
 
 typedef struct screen {
     int pid;
-    char* name;
-    char* state; // Detached/Attached
-    char* date;
+    char *name;
+    char *state; // Detached/Attached
+    char *date;
 } screen;
 
 /* Is initted by compiler.
  */
 struct {
    bool create;
-   char* next_arg;
+   char *next_arg;
 
-   char* regex_or_name;
+   char *regex_or_name;
 
    char hostname[LIMIT_HOSTNAME];
 
@@ -53,18 +56,20 @@ struct {
     *    vec: [ screen, screen, screen, ... ], // attached
     * }
     */
-   vec* data[2];
+   vec *data[2];
 
-   int* pids;
-   char** names;
+   int *pids;
+   char **names;
 
    int total_num;
 
    char *rl_l;
    size_t rl_s; // no init necessary
+
+   int version;
 } g;
 
-error_t argp_parser(int key, char* arg, struct argp_state *state) {
+error_t argp_parser(int key, char *arg, struct argp_state *state) {
     if (key == 'n') {
         g.create = true;
     }
@@ -101,6 +106,8 @@ static struct argp_option options[] = {
     {0}
 };
 
+int get_screen_version();
+
 void data_init() {
     g.data[0] = vec_new();
     g.data[1] = vec_new();
@@ -108,8 +115,8 @@ void data_init() {
 
 // 0 -- detached
 // 1 -- attached
-void data_push(int which, screen* s) {
-    vec* v = g.data[which];
+void data_push(int which, screen *s) {
+    vec *v = g.data[which];
     vec_add(v, s);
 }
 
@@ -126,24 +133,42 @@ void cleanup() {
 void load_data() {
     sys_die(true);
 
-    FILE* f = sysr("screen -ls");
+    FILE *f = sysr("screen -ls");
 
     while (getline(&g.rl_l, &g.rl_s, f) != -1) {
-        char* matches[5] = {0};
+        char *matches[5] = {0};
 
-        char* re = "^ \\s* (\\d+) \\. (.+?) \\s+ ( \\(.+?\\) ) \\s+ ( \\(.+?\\) ) ";
+        // Date only in newer screen, and anyway not used.
+        char *re, *name, *date, *state;
+        int pid;
+        if (g.version == VERSION_OLD) {
+            re = "^ \\s* (\\d+) \\. (.+?) \\s+ ( \\( .+? \\) ) "; //x
 
-        if (! match_matches_flags(g.rl_l, re, matches, PCRE_EXTENDED)) //x
-            continue;
+            if (! match_matches_flags(g.rl_l, re, matches, PCRE_EXTENDED)) 
+                continue;
 
-        // malloc'd => ok to store
-        int pid = atoi(matches[1]);
-        char* name = matches[2];
-        char* date = matches[3];
-        char* state = matches[4];
+            // malloc'd => ok to store
+            pid = atoi(matches[1]);
+            name = matches[2];
+            state = matches[3];
+        }
+        else if (g.version == VERSION_NEW) {
+            re = "^ \\s* (\\d+) \\. (.+?) \\s+ ( \\(.+?\\) ) \\s+ ( \\(.+?\\) ) "; //x
+            if (! match_matches_flags(g.rl_l, re, matches, PCRE_EXTENDED)) 
+                continue;
+
+            // malloc'd => ok to store
+            pid = atoi(matches[1]);
+            name = matches[2];
+            date = matches[3];
+            state = matches[4];
+        }
+        else {
+            piepr;
+        }
 
         debug("name, %s", name);
-        debug("date, %s", date);
+        if (date) debug("date, %s", date);
         debug("state, %s", state);
         debug("pid, %d", pid);
 
@@ -178,7 +203,7 @@ void load_data() {
     pclose(f);
 }
 
-void check_has_match(bool* has_match, bool* exec, bool* tried_new) {
+void check_has_match(bool *has_match, bool *exec, bool *tried_new) {
     if (vec_size(g.data[0]) == 0 && vec_size(g.data[1]) == 0) {
         debug("nothing.");
 
@@ -204,12 +229,12 @@ void check_has_match(bool* has_match, bool* exec, bool* tried_new) {
     }
 }
 
-char* menu_field(int length, int pid, int state) {
+char *menu_field(int length, int pid, int state) {
     int len = int_length(pid);
-    char* s = str(len + 1);
+    char *s = str(len + 1);
     sprintf(s, "%d", pid);
     
-    char* f = f_field(length, s, len + 1);
+    char *f = f_field(length, s, len + 1);
     _();
     if (state == 0) CY(f);
     else BR(f);
@@ -217,23 +242,23 @@ char* menu_field(int length, int pid, int state) {
     free(s);
     free(f);
 
-    char* ret = str(strlen(_s) + 1);
+    char *ret = str(strlen(_s) + 1);
     strcpy(ret, _s);
     return ret;
 }
 
-void menu_line(int idx, int pid, char* name, int state) {
-    char* field = menu_field(6, pid, state);
+void menu_line(int idx, int pid, char *name, int state) {
+    char *field = menu_field(6, pid, state);
     _();
     Y(name);
-    printf("%2d. %s%s\n", idx, field, _s);
+    printf("%2d. %s %s\n", idx, field, _s);
     free(field);
 }
 
 void menu() {
     int idx = 0;
     for (int which = 0; which < 2; which++) {
-        vec* v = g.data[which];
+        vec *v = g.data[which];
         for (int i = 0; i < vec_size(v); i++) {
             idx++;
             screen *scr = vec_get(v, i);
@@ -254,7 +279,7 @@ int load_pids_and_names() {
     int idx = -1;
     for (int which = 0; which < 2; which++) {
         for (int i = 0; i < n[which]; i++) {
-            vec* v = g.data[which];
+            vec *v = g.data[which];
             screen *scr = (screen*) vec_get(v, i);
             idx++;
             debug("idx %d, which %d, storing pid %d, name %s", idx, which, scr->pid, scr->name);
@@ -267,7 +292,7 @@ int load_pids_and_names() {
     return num;
 }
 
-void screen_new(char* name) { // taint
+void screen_new(char *name) { // taint
     // use system path
     try(
         execlp("screen", "screen", "-S", name, NULL)
@@ -276,7 +301,7 @@ void screen_new(char* name) { // taint
 
 void screen_go(int pid) {
     debug("going to pid %d", pid);
-    char* p = str(int_length(pid) + 1);
+    char *p = str(int_length(pid) + 1);
     sprintf(p, "%d", pid);
     try(
         execlp("screen", "screen", "-rd", p, NULL)
@@ -291,6 +316,10 @@ void init() {
     verbose_cmds(false);
 #endif
     g.rl_l = NULL;
+
+    g.version = get_screen_version();
+    if (!g.version)
+        err("Couldn't get screen version.");
 
     g.total_num = 0;
     
@@ -337,7 +366,7 @@ int get_sel() {
         // regex match
         else {
             for (int i = 0; i < num; i++) {
-                char* name = g.names[i];
+                char *name = g.names[i];
                 debug("checking name: %s", name);
                 // take first match
                 if (match(name, g.rl_l)) {
@@ -350,12 +379,12 @@ int get_sel() {
     return sel;
 }
 
-void _exit(status) {
+void _exit(int status) {
     cleanup();
     exit(status);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     init();
 
     /* Can also init with: static struct argp args */
@@ -428,6 +457,34 @@ int main(int argc, char** argv) {
     int pid = g.pids[sel];
     debug("Got sel: %d, pid %d", sel, pid);
     screen_go(pid); //exec, how to cleanup? XX
+}
+
+int get_screen_version() {
+    int version_maj, version_min = -1;
+
+    char *cmd = "screen -v";
+    sys_die(0); // exit value is 1 for some reason
+    FILE *f = sysr(cmd);
+    if (!f) return 0;
+    char *re = "^ Screen \\s+ version \\s+ (\\d+) \\. (\\d+) "; //x
+
+    char *matches[3];
+    while (getline(&g.rl_l, &g.rl_s, f) != -1) {
+        if (match_matches_flags(g.rl_l, re, matches, PCRE_EXTENDED))  {
+            char *v1 = matches[1];
+            char *v2 = matches[2];
+            version_maj = atoi(v1);
+            version_min = atoi(v2);
+            //info ("Maj %d min %d", version_maj, version_min);
+            break;
+        }
+    }
+    return 
+        version_maj == -1 ? 0 :
+        version_min == -1 ? 0 :
+        version_maj < 4 ? VERSION_OLD :
+        (version_maj == 4 && version_min < 2) ? VERSION_OLD :
+        VERSION_NEW;
 }
 
 
